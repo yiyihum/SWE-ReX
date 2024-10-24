717cdc51c718442f5feee9d1db5a0caf9fc697e8
Kilian Lieret <kilian.lieret@posteo.de>
2024-10-24 11:45:10 -0400

    Fix: Ensure authentication with modal (#103)
    * Fix: Ensure authentication with modal
    
    * Test what's going on with failing installation
    
    * Revert "Test what's going on with failing installation"
    
    This reverts commit 291ac38c3b63607d9190615983b3e47b593125b7.
    
    * Fix  param name

--- src/swerex/deployment/modal.py
+++ src/swerex/deployment/modal.py
@@ -1,6 +1,7 @@
 import asyncio
 import os
 import time
+import uuid
 from pathlib import Path, PurePath
 from typing import Any
 
@@ -123,6 +124,10 @@ class ModalDeployment(AbstractDeployment):
             modal_sandbox_kwargs = {}
         self._modal_kwargs = modal_sandbox_kwargs
 
+    def _get_token(self) -> str:
+        # generate random token
+        return str(uuid.uuid4())
+
     async def is_alive(self, *, timeout: float | None = None) -> IsAliveResponse:
         if self._runtime is None or self._sandbox is None:
             raise DeploymentNotStartedError()
@@ -138,12 +143,12 @@ class ModalDeployment(AbstractDeployment):
         assert self._runtime is not None
         return await _wait_until_alive(self.is_alive, timeout=timeout, function_timeout=self._runtime._timeout)
 
-    def _start_swerex_cmd(self) -> str:
+    def _start_swerex_cmd(self, token: str) -> str:
         """Start swerex-server on the remote. If swerex is not installed arelady,
         install pipx and then run swerex-server with pipx run
         """
         pkg_name = "0fdb5604"
-        return f"{REMOTE_EXECUTABLE_NAME} --port {self._port} || pipx run {pkg_name} --port {self._port}"
+        return f"{REMOTE_EXECUTABLE_NAME} --port {self._port} --api-key {token} || pipx run {pkg_name} --port {self._port} --api-key {token}"
 
     def get_modal_log_url(self) -> str:
         """Returns URL to modal logs"""
@@ -156,10 +161,11 @@ class ModalDeployment(AbstractDeployment):
     ):
         self.logger.info("Starting modal sandbox")
         t0 = time.time()
+        token = self._get_token()
         self._sandbox = modal.Sandbox.create(
             "/bin/bash",
             "-c",
-            self._start_swerex_cmd(),
+            self._start_swerex_cmd(token),
             image=self._image,
             timeout=int(self._container_timeout),
             unencrypted_ports=[self._port],
@@ -172,7 +178,7 @@ class ModalDeployment(AbstractDeployment):
         self.logger.info(f"Sandbox created with id {self._sandbox.object_id}")
         await asyncio.sleep(1)
         self.logger.info(f"Starting runtime at {tunnel.url}")
-        self._runtime = RemoteRuntime(host=tunnel.url, timeout=self._runtime_timeout)
+        self._runtime = RemoteRuntime(host=tunnel.url, timeout=self._runtime_timeout, token=token)
         t0 = time.time()
         await self._wait_until_alive(timeout=timeout)
         self.logger.info(f"Runtime started in {time.time() - t0:.2f}s")
