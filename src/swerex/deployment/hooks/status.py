a526fdf6332bba99df0c404c7da70171f7bd0429
Kilian Lieret <kilian.lieret@posteo.de>
2024-11-18 18:43:18 -0500

    Fix type annotation for callable

--- src/swerex/deployment/hooks/status.py
+++ src/swerex/deployment/hooks/status.py
@@ -4,12 +4,12 @@ from swerex.deployment.hooks.abstract import DeploymentHook
 
 
 class SetStatusDeploymentHook(DeploymentHook):
-    def __init__(self, id: str, callable: Callable[[str], None]):
+    def __init__(self, id: str, callable: Callable[[str, str], None]):
         self._callable = callable
         self._id = id
 
     def _update(self, message: str):
-        self._callable(self._id, message)
+        self._callable(self._id, message)  # type: ignore
 
     def on_custom_step(self, message: str):
         self._update(message)
